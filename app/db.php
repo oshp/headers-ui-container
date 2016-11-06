43c94c7a7b08e82f765473e176951cb2d0bfebb0
alexandre menezes <alexandre.fmenezes@gmail.com>
2016-11-06 20:23:22 -0200

    Create db.php

---
+++ app/db.php
@@ -0,0 +1,15 @@
+<?php
+  $conn = NULL;
+  class DB {
+    function getConnection() {
+      #if (is_null($GLOBALS['conn'])) {
+        $mysqli = new mysqli("mysql", "root", "password", "headers");
+        if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_error());
+            #exit();
+        }
+      #}
+      return  $mysqli;
+    }
+  }
+ ?>
